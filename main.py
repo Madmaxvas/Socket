b8c32147591c646c5142f7e743cfb0a527ce6006
maxmadvas <maxmadvas@gmail.com>
2024-07-04 02:39:03 +0300

    Готово

--- main.py
+++ main.py
@@ -3,12 +3,15 @@ from flask_login import LoginManager
 from flask_login import LoginManager, login_required
 from flask_login import login_user, logout_user, current_user
 from tools.forms import LoginForm, RegisterForm, Videoupload
-from flask import render_template, flash, redirect, url_for, request
+from flask import render_template, flash, redirect, url_for, request, jsonify, make_response
 from flask_bs4 import Bootstrap
 from misc import db
 from tools.db import User
 import os
+import time
 import shortuuid
+from  time import  strftime
+
 SECRET_KEY = os.urandom(32)
 
 
@@ -33,15 +36,23 @@ if not a:
     db.commit()
     db_cursor.execute('''CREATE TABLE videous (          
                                                 videoname TEXT PRIMARY KEY,
+                                                name TEXT,
                                                 username TEXT,
                                                 fotos TEXT,
                                                 opis TEXT,
-                                                tags TEXT);''')
+                                                tags TEXT,
+                                                time TEXT);''')
     db.commit()
 db_cursor.close()
 
 
 
+
+@login_required
+@app.route('/logout')
+def logout():
+    logout_user()
+    return redirect(url_for('lologin'))
 
 
 @login_required
@@ -55,16 +66,22 @@ def upload():
         for i in out:
             if current_user.username in i:
                     vstav1 = shortuuid.ShortUUID().random(length=22)
-                    form1.video.data.save('static/videous/' + vstav1 + '.'+ form1.video.data.filename.split('.')[1])
-                    form1.photo.data.save('static/images/' + vstav1 +'.'+ form1.photo.data.filename.split('.')[1])
+                    form1.video.data.save('static/videous/' + vstav1 + '.'+ 'mp4')#form1.video.data.filename.split('.')[1])
+                    form1.photo.data.save('static/images/' + vstav1 +'.'+ 'png')#form1.photo.data.filename.split('.')[1])
                     photovst = vstav1
-                    db_cursor.execute('''INSERT or IGNORE INTO videous (videoname,username,fotos,opis,tags) VALUES (?, ?, ?, ?, ?)''', (vstav1,current_user.username,photovst,form1.opis.data,form1.tags.data))
+                    db_cursor.execute('''INSERT or IGNORE INTO videous (name,videoname,username,fotos,opis,tags,time) VALUES (?, ?, ?, ?, ?, ?, ?)''', (form1.name.data,vstav1,current_user.username,photovst,form1.opis.data,form1.tags.data,strftime('%H:%M:%S:%j:%Y')))
         db.commit()
         db_cursor.close()
     return render_template('upload.html', form=form1)
 
+@login_required
+@app.route('/test', methods=['GET','POST'])
+def test():
+    return render_template('test.html')
 
-@app.route('/login', methods=['GET', 'POST'])
+
+
+@app.route('/', methods=['GET', 'POST'])
 def lologin():
     form = LoginForm()
     if form.validate_on_submit():
@@ -77,7 +94,7 @@ def lologin():
             if i[1] == form.username.data and i[3] == form.password.data:
                 user = User(i[0], i[1], i[2], i[3])
                 login_user(user=user)
-                return render_template('test.html', title='Вход')
+                return redirect(url_for('spiso'))
         flash("Ошибочный username или password")
         return redirect(url_for('lologin'))
     return render_template('login.html', title='Вход', form=form)
@@ -92,11 +109,9 @@ def register():
         out = db_cursor.fetchall()
         for i in out:
             if i[0] == form.email.data:
-                print("Данный email уже зарегистрирован!")
                 db_cursor.close()
                 return redirect(url_for('register'))
             elif i[1] == form.username.data:
-                print("Данный UserName уже зарегистрирован")
                 db_cursor.close()
                 return redirect(url_for('register'))
 
@@ -112,16 +127,31 @@ def register():
 
 @login.user_loader
 def load_user(user_id):
-    print(f"login:{user_id}")
     db_cursor1 = db.cursor()
     db_cursor1.execute("SELECT * FROM info WHERE username=(?)", (user_id,))
     db_data = db_cursor1.fetchone()
-    print(db_data)
     db_cursor1.close()
     if db_data is not None:
         return User(db_data[0], db_data[1], db_data[2], db_data[3])
     else:
         return None
+
+
+
+@login_required
+@app.route('/spiso', methods=['GET','POST'])
+def spiso():
+    if request.method == 'POST':
+        inf = request.form.get('2')
+        return render_template('test.html', inf = inf)
+    db_cursor = db.cursor()
+    db_cursor.execute("SELECT name,fotos FROM videous")
+    info = db_cursor.fetchall()
+    db_cursor.close()
+    return render_template('load.html', info = info)
+
+
+
 
 if __name__ == '__main__':
     app.run()
